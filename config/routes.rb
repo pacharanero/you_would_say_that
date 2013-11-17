b7dd1e9003785f2297d19b1f826f240e1af36154
pacharanero <marcusbaw@gmail.com>
2013-11-17 13:58:45 +0000

    general furtling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ YouWouldSayThat::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'statements#index'
+  root 'welcome/welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
