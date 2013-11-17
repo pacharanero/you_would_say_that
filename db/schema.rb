36fcc2ec1304220da94b0df7a084122c4710fdb4
pacharanero <marcusbaw@gmail.com>
2013-11-17 13:05:27 +0000

    registration page tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 20131116212227) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
